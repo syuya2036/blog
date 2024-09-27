7cc55eaca18c68d4f662e535e6d5844569ff8993
syuya2036 <f20065@g.ichinoseki.ac.jp>
2024-09-27 19:08:14 +0900

    フッターの作成

---
+++ components/site-footer.tsx
@@ -0,0 +1,22 @@
+export default function SiteFooter() {
+  return (
+    <footer className="w-full py-8 text-center">
+      <div className="container mx-auto">
+        <div className="mb-2">
+          <a href="#" className="text-gray-400 mx-2 hover:text-white">
+            Twitter
+          </a>
+          <a href="#" className="text-gray-400 mx-2 hover:text-white">
+            Instagram
+          </a>
+          <a href="#" className="text-gray-400 mx-2 hover:text-white">
+            Facebook
+          </a>
+        </div>
+        <p className="text-gray-500 text-sm">
+          &copy; 2024 MyCompany. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
